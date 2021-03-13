3bb1f3902646b24fbf6866b5ef2c497a82915e25
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-03-13 22:48:30 +0900

    finish Progate's php_ chap1

---
+++ Progate/PHP/php_1/php_1_13.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+
+    // 変数$iを定義してください
+    $i = 2;
+    
+    // while文を書いてください
+    while ($i <= 50) {							// 保守性を欠いている　条件分岐にしてみよう↓
+			echo $i * 2 . "<br>";
+			$i ++;
+		}
+    
+		// while ($i <= 100) {
+		  // if($i % 2 == 0) {
+			//echo $i . "<br>";
+		//	}
+		// 	$i ++;
+		//}
+
+  ?>
+
+</body>
+</html>
+
---
+++ Progate/PHP/php_1/php_1_7.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Progate</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+
+  <?php
+    // $ageという変数に自分の年齢を代入してください
+    $sage = 28;
+		if ($sage >= 30) {
+			echo "あなたは３０歳以上です。";
+		}else{
+			echo "あなたは３０歳未満です。";
+		}
+    
+  ?>
+
+</body>
+</html>
