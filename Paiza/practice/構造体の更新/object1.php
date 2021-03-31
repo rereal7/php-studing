adc5444da26f6e1a464b5ca20c759f604fb24493
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-03-31 19:33:32 +0900

    Update after review

--- Paiza/practice/構造体の更新/object1.php
+++ Paiza/practice/構造体の更新/object1.php
@@ -3,18 +3,23 @@
 //与えられる人数の値を取得
 $personCount = trim(fgets(STDIN));
 
-//各情報の出力
-for ($i = 0; $i < $personCount; $i++) {
+// 各情報の読み込み
+// for文でぶん回して、構造体に格納する
+$users = [];
+for ($i = 0;$i < $personCount ; $i++) {
 	$input_line = trim(fgets(STDIN));
 	$inputs = explode(" ", $input_line);
-	$name = $inputs[0];
-	$old = $inputs[1];
-	$birth = $inputs[2];
-	$state = $inputs[3];
+	
+	$users[] = array("nickname" => $inputs[0], "old" => $inputs[1], "birth" => $inputs[2], "state" => $inputs[3]);
+}
+
+// 各情報の書き出し
+// 構造体にあるデータを一気に表示する
+for ($i = 0;$i < $personCount ; $i++) {
 	echo "User{\n";
-	echo "nickname : " . $name . "\n";
-	echo "old : " . $old . "\n";
-	echo "birth : " . $birth . "\n";
-	echo "state : " . $state . "\n";
+	echo "nickname : " . $users[$i]['nickname'] . "\n";
+	echo "old : " . $users[$i]['old'] . "\n";
+	echo "birth : " . $users[$i]['birth'] . "\n";
+	echo "state : " . $users[$i]['state'] . "\n";
 	echo "}\n";
 }
